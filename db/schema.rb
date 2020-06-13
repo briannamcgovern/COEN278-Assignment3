c203da2d860d8addc722e3ffa2dd905494240f65
Brianna McGovern <bmcgovern007@gmail.com>
2020-06-13 11:22:18 -0700

    Changes to be committed: 	modified:   Gemfile 	modified:   Gemfile.lock 	modified:   db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -12,14 +12,17 @@
 
 ActiveRecord::Schema.define(version: 2020_06_13_041212) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "line_items", force: :cascade do |t|
-    t.integer "plant_id", null: false
-    t.integer "cart_id", null: false
+    t.bigint "plant_id", null: false
+    t.bigint "cart_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "quantity", default: 1
